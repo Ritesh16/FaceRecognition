801ace07d182ea4fec4b778174e3cb861ffe942c
Ritesh16 <ritesh.sharma.cse@gmail.com>
2020-09-06 16:11:02 -0400

    adding app to heroku

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component{
 
       onButtonSubmit = () =>{
         this.setState({imageUrl: this.state.input});
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://stark-shore-03611.herokuapp.com/imageurl', {
           method:'post',
           headers: {'Content-Type' : 'application/json'},
           body: JSON.stringify({
@@ -97,7 +97,7 @@ class App extends Component{
             .then(response => 
               {
                 if(response){
-                  fetch('http://localhost:3000/image', {
+                  fetch('https://stark-shore-03611.herokuapp.com/image', {
                     method:'put',
                     headers: {'Content-Type' : 'application/json'},
                     body: JSON.stringify({
